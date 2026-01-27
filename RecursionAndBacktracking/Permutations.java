02e2a9ab9c9dd973af6599f3103a348bfdc900ac
Samaksh <samaksh199@gmail.com>
2026-01-27 17:02:23 +0530

    Solved Permutations

--- RecursionAndBacktracking/Permutations.java
+++ RecursionAndBacktracking/Permutations.java
@@ -3,11 +3,11 @@ class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
         boolean[] stat = new boolean[nums.length];
-        backtrack(0, nums, stat, curr, ans);
+        backtrack(nums, stat, curr, ans);
         return ans;
     }
 
-    public void backtrack(int start, int[] nums, boolean[] stat, List<Integer> curr, List<List<Integer>> ans) {
+    public void backtrack(int[] nums, boolean[] stat, List<Integer> curr, List<List<Integer>> ans) {
         if (curr.size() == nums.length) {
             ans.add(new ArrayList<>(curr));
         }
@@ -20,7 +20,7 @@ class Solution {
                 stat[i] = true;
                 curr.add(nums[i]);
             }
-            backtrack(i + 1, nums, stat, curr, ans);
+            backtrack(nums, stat, curr, ans);
             curr.remove(curr.size() - 1);
             stat[i] = false;
         }
